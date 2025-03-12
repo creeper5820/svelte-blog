3cf46fb10cb5d5d0710f659c713c4f91bfa70715
creeper5820 <cqq1960180796@163.com>
2025-03-12 10:17:40 +0800

    update

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,26 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+    namespace App {
+        // interface Error {}
+        // interface Locals {}
+        // interface PageData {}
+        // interface PageState {}
+
+        interface Platform {
+            env: {
+                COUNTER: DurableObjectNamespace;
+            };
+            context: {
+                waitUntil(promise: Promise<any>): void;
+            };
+            caches: CacheStorage & { default: Cache }
+        }
+
+        export async function post(context) {
+            const counter = context.platform.env.COUNTER.idFromName("A");
+        }
+    }
 }
 
-export {};
+export { };
